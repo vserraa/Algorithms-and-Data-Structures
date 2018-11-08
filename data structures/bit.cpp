f880b7775a085d75682410c9ca4fd003003243e0
Vinicius Serra <vssm@cin.ufpe.br>
2018-11-08 00:42:33 -0200

    'first_ge'

--- data structures/bit.cpp
+++ data structures/bit.cpp
@@ -1,7 +1,8 @@
 #include <bits/stdc++.h>
 using namespace std;
+typedef long long ll;
 const int ms = 1e5+10; 
-int a[ms], bit[ms];
+int a[ms], bit[ms], n;
 
 void updt(int i, int val){
 	a[i] += val;
@@ -9,16 +10,28 @@ void updt(int i, int val){
 		bit[i] += val;
 }
 
-long long query(int i){
-	long long ans = 0;
+ll query(int i){
+	ll ans = 0;
 	for(; i > 0; i -= (i&-i))
 		ans += bit[i];
 	return ans;
 }
 
+int find_first_ge(ll val){
+	int pos = 0;
+	ll now = 0;
+	for(int i = 1<<19; i > 0; i >>= 1){
+		if(pos + i <= n && now + bit[pos+i] < val){
+			now += bit[pos+i];
+			pos += i;
+		}
+	}
+
+	return pos; //1-indexed 
+}
+
 int main(){
 	ios::sync_with_stdio(0), cin.tie(0);
-	int n;
 	cin >> n;
 	for(int i = 1; i <= n; i++){
 		cin >> a[i];

e274b91b15eb98c62bb944017e1eb46aace3da63
Vinicius Serra <vssm@cin.ufpe.br>
2019-08-29 00:06:18 -0300

    'SOS'

---
+++ variety/SOSdp.cpp
@@ -0,0 +1,17 @@
+//sum of A[i] in SUBSETS of mask
+for(int i = 0; i<(1<<N); ++i)
+	F[i] = A[i];
+for(int i = 0;i < N; ++i) for(int mask = 0; mask < (1<<N); ++mask){
+	if(mask & (1<<i))
+		F[mask] += F[mask^(1<<i)];
+}
+
+
+
+//sum of A[i] over all SUPERMASKS of mask
+for(int i = 0; i<(1<<N); ++i)
+	F[i] = A[i];
+for(int i = 0;i < N; ++i) for(int mask = 0; mask < (1<<N); ++mask){
+	if(mask & (1<<i))
+		F[mask^(1<<i)] += F[mask];
+}

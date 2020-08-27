e63125d2eef29f6abd18927529870940fff2d939
Vinícius Serra <vssm@cin.ufpe.br>
2020-08-27 12:58:24 -0300

    palindromic tree

---
+++ strings/eertree.cpp
@@ -0,0 +1,107 @@
+#include <cmath>
+#include <functional>
+#include <fstream>
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string>
+#include <set>
+#include <map>
+#include <list>
+#include <time.h>
+#include <math.h>
+#include <random>
+#include <deque>
+#include <queue>
+#include <cassert>
+#include <unordered_map>
+#include <unordered_set>
+#include <iomanip>
+#include <bitset>
+#include <sstream>
+#include <chrono>
+#include <cstring>
+#include <cstdlib>
+ 
+using namespace std;
+ 
+typedef long long ll;
+const int sigma = 26;
+ 
+struct Eertree {
+    struct Node {
+        int id, len, link;
+        vector<int> to;
+        Node(): id(0), len(0), link(0) {
+            to.assign(sigma, -1);
+        };
+        Node(int id, int len, int link): id(id), len(len), link(link) {
+            to.assign(sigma, -1);
+        };
+    };
+    string T;
+    vector<Node> nodes;
+    int lsp, nxt;
+ 
+    Eertree() {
+        nodes.push_back(Node(-1, -1, 0));
+        nodes.push_back(Node(0, 0, 0));
+        lsp = 0, nxt = 2;
+    }
+ 
+    int add(char c) {
+        int size = T.size();
+        int cur = lsp;
+              
+        while(nodes[cur].id != -1) {
+            if(size-1-nodes[cur].len >= 0 && T[size -1 -nodes[cur].len] == c)
+                break;
+            cur = nodes[cur].link;            
+        }
+ 
+        if(nodes[cur].to[c-'a'] != -1) {
+            lsp = nodes[cur].to[c-'a'];
+            T.push_back(c);
+            return 0;
+        }
+ 
+        Node new_node(size+2, nodes[cur].len+2, -1);
+        nodes[cur].to[c-'a'] = nxt;
+        lsp = nxt++;
+ 
+        if(new_node.len == 1) {
+            new_node.link = 1;
+            nodes.push_back(new_node);
+            T.push_back(c);
+            return 1;
+        }
+        
+        cur = nodes[cur].link;
+        while(nodes[cur].id != -1) {
+            if(size-1-nodes[cur].len >= 0 && T[size -1 -nodes[cur].len] == c)
+                break;
+            cur = nodes[cur].link;  
+        }
+ 
+        if(nodes[cur].to[c-'a'] != -1) 
+            new_node.link = nodes[cur].to[c-'a'];
+        else 
+            new_node.link = 0;
+ 
+        nodes.push_back(new_node);
+        T.push_back(c);
+ 
+        return 1;
+    }
+};
+
+int main() {
+    ios::sync_with_stdio(0), cin.tie(0);
+    string s;
+    cin >> s;
+    Eertree palindromic_tree;
+    for(auto v : s) {
+        cout << palindromic_tree.add(v) << endl;
+    }
+    return 0;
+}
